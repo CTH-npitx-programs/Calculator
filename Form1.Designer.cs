49b371c598321ca8932145b7e00471af7912faa1
CTH-npitx <chatch@npitx.org>
2024-08-07 12:22:01 -0500

    begin close system, add debug list, debug list not work, remove debug list

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,14 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.bttnClose = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.tmr_ConfirmClose = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // bttnClose
             // 
             this.bttnClose.BackColor = System.Drawing.Color.Red;
             this.bttnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.bttnClose.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.bttnClose.FlatAppearance.BorderSize = 0;
             this.bttnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
@@ -43,7 +45,7 @@
             this.bttnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.bttnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bttnClose.ForeColor = System.Drawing.Color.Black;
-            this.bttnClose.Location = new System.Drawing.Point(696, 20);
+            this.bttnClose.Location = new System.Drawing.Point(714, 1);
             this.bttnClose.Name = "bttnClose";
             this.bttnClose.Size = new System.Drawing.Size(85, 43);
             this.bttnClose.TabIndex = 0;
@@ -51,20 +53,14 @@
             this.bttnClose.UseVisualStyleBackColor = false;
             this.bttnClose.Click += new System.EventHandler(this.bttnClose_Click);
             // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(490, 1);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 1;
-            // 
             // frmCalc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.CancelButton = this.bttnClose;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.bttnClose);
             this.HelpButton = true;
             this.MaximizeBox = false;
@@ -73,7 +69,7 @@
             this.Padding = new System.Windows.Forms.Padding(16);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculator Inator";
-            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.frmCalc_Load);
             this.ResumeLayout(false);
 
         }
@@ -81,7 +77,7 @@
         #endregion
 
         private System.Windows.Forms.Button bttnClose;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer tmr_ConfirmClose;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,10 +15,45 @@ namespace Calculator
         public frmCalc()
         {
             InitializeComponent();
+
+
+        }
+        bool debug = true;
+        private void frmCalc_Load(object sender, EventArgs e)
+        {
+            lst_TextStore.Items.Clear();
+            if( debug == true)
+            {
+                lst_TextStore.Visible = true;
+            } //if debug on, see list
+
+            string addItem(string item, bool newList)
+            {
+                if (newList == true)
+                {
+                    string empty = string.Empty;
+                    lst_TextStore.Items.Add(item);
+                    lst_TextStore.Items.Add(empty);
+                } else
+                {
+                    lst_TextStore.Items.Add(item);
+                }
+                return lst_TextStore.Items.ToString();
+            };
+
+            string closeText = bttnClose.Text;
+            string confirmClose = "Please Confirm Your Desire to Leave";
+
+            addItem(debug.ToString(), false); //debug mode?
+            addItem(closeText, true); //close text
+            addItem(confirmClose, false); //confirm close
         }
 
         private void bttnClose_Click(object sender, EventArgs e)
         {
+        
+
+            
 
         }
     }
