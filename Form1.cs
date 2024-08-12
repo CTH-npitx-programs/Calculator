688afc31ec92aa7a0eaf26e9ad426ea5e1dc00f2
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2024-08-12 11:01:33 -0500

    start work on trying to make changeing button system

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -29,33 +30,48 @@ namespace Calculator
             } //if debug on see list
             else {
                 this.Size = new Size(816, 489);
-            }// if deug off, shrink form for conscisness
-
-            string addItem(string item, bool newEntry)
-            {
-                string empty = Environment.NewLine;
-                if (newEntry == true)
-                {
-                    txt_debug.AppendText(empty);
-                } else
-                {
-                }
-                txt_debug.AppendText(item);
-                txt_debug.AppendText(empty);
-                return txt_debug.Text.ToString();
-            };
+            }// if deug off, shrink form for conscisness;
 
             string closeText = bttnClose.Text;
             string confirmClose = "Please Confirm Your Desire to Leave";
 
+            
+
             addItem(debug.ToString(),false); //debug mode?
             addItem(closeText, true); //close text
             addItem(confirmClose, false); //confirm close
+
+            string getNextName()
+            {
+                if (bttnClose.Text == closeText)
+                {
+                    return confirmClose;
+                }
+                else
+                {
+                    return closeText;
+                }
+            }
         }
 
+
+
+        private string addItem(string item, bool newEntry)
+        {
+            string empty = Environment.NewLine;
+            if (newEntry == true)
+            {
+                txt_debug.AppendText(empty);
+            }
+            else
+            {
+            }
+            txt_debug.AppendText(item);
+            txt_debug.AppendText(empty);
+            return txt_debug.Text.ToString();
+        }
         private void bttnClose_Click(object sender, EventArgs e)
         {
-        
 
             
 
