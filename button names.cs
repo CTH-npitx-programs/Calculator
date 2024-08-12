7d3a517a15f6ccb2389251a53c425b44ef276485
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2024-08-12 11:26:09 -0500

    simple class

---
+++ button names.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class button_names
+    {
+        public button_names()
+        {
+            
+        }
+    }
+}
